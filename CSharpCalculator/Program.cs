7ccf99f58b14076a77b8368b6dbfcb5291baec00
smbirch <spencer.m.birch@gmail.com>
2021-05-07 01:39:35 -0700

    Adding logic to prevent division by zero

--- CSharpCalculator/Program.cs
+++ CSharpCalculator/Program.cs
@@ -35,15 +35,18 @@ namespace CSharpCalculator
                     Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
                     break;
                 case "d":
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("Enter a non-zero divisor: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                    }
                     Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
                     break;
             } 
+
             Console.WriteLine("Press any key to close the calculator");
             Console.ReadKey();
 
         }
     }
 }
-//testing
-//testing
-//testing
